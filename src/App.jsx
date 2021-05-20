4aca264119fe97c41f78578234da633c5e600227
Amila Roshan <amila.roshan@saksglobal.com>
2021-05-20 15:34:16 +0530

    scaffold

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-indigo-200 min-h-screen ">
+
+    </div>
+  )
+}
+
+export default App
